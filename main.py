9de5628fa728d95c33bb3bea073276019992166c
keshavsharma25 <skeshav0825@gmail.com>
2025-10-17 00:42:45 +0530

    op_sepolia deposit eth & deposit transactions
    script successfully executes both eth deposits & contract interactions
    via L1

--- main.py
+++ main.py
@@ -1,86 +1,37 @@
-from typing import cast
-from eth_account.signers.local import LocalAccount
-from pprint import pprint
-from web3 import Account
+from web3 import Web3
+import json
 from chains.op_stack.op_sepolia import OP_Sepolia
-from dotenv import load_dotenv
-import os
 
 
 def main():
-    load_dotenv()
-
     op_sepolia = OP_Sepolia()
+    l2p = op_sepolia.l2p
 
-    pvt_key = os.getenv("PRIVATE_KEY")
+    #### deposit ETH ####
 
-    if not pvt_key:
-        raise ValueError("PRIVATE_KEY not set in .env...")
+    receipt = op_sepolia.deposit_eth(op_sepolia.account.address, 0.01)
+    print(receipt)
 
-    account: LocalAccount = Account.from_key(pvt_key)
+    #### deposit transactions ####
 
-    contract = op_sepolia.get_l1_contract()
-
-    address = account.address
-    value = op_sepolia.l1p.to_wei(0.01, "ether")
-    gas_limit = op_sepolia.l2p.to_wei(0.01, "gwei")
-    is_creation = False
-    data = op_sepolia.l1p.to_bytes(b"")
-
-    txn = contract.functions.depositTransaction(
-        address,
-        value,
-        gas_limit,
-        is_creation,
-        data,
+    contract_address = Web3.to_checksum_address(
+        "0x5fd84259d66Cd46123540766Be93DFE6D43130D7"
     )
 
-    estimated_gas = op_sepolia.l1p.eth.estimate_gas(
-        {
-            "from": account.address,
-            "to": contract.address,
-            "value": value,
-            "data": txn._encode_transaction_data(),
-        }
+    addr_2 = Web3.to_checksum_address("0x15dbD2206F4aa38E7e6D11454270E00FB3568F1c")
+
+    with open("tests/op_stack/mocks/usdc_op_sepolia.json", "r") as file:
+        abi = json.load(file)
+
+    contract = l2p.eth.contract(address=contract_address, abi=abi)
+
+    calldata = contract.functions.transfer(addr_2, 1000000)._encode_transaction_data()
+
+    receipt = op_sepolia.deposit_transaction(
+        to=contract_address, value=0, is_creation=False, data=calldata
     )
 
-    txn_payload = txn.build_transaction(
-        {
-            "from": account.address,
-            "chainId": op_sepolia.l1p.eth.chain_id,
-            "gas": estimated_gas,
-            "value": value,
-            "nonce": op_sepolia.l1p.eth.get_transaction_count(account.address),
-        }
-    )
-
-    pprint(txn_payload)
-
-    print("=" * 20)
-
-    print("Sending Txn....")
-
-    print("=" * 20)
-
-    signed_txn = account.sign_transaction(cast(dict, txn_payload))
-    txn_hash = op_sepolia.l1p.eth.send_raw_transaction(signed_txn.raw_transaction)
-
-    print("=" * 20)
-    print(f"Txn hash: {txn_hash.hex()}")
-    print("=" * 20)
-
-    receipt = op_sepolia.l1p.eth.wait_for_transaction_receipt(txn_hash)
-
-    if receipt["status"] == 1:
-        print("✅ Transaction successful!")
-        print(f"Block: {receipt['blockNumber']}")
-        print(f"Gas used: {receipt['gasUsed']}")
-
-        print("=" * 20)
-    else:
-        print("❌ Transaction failed!")
-
-        return receipt
+    print(receipt)
 
 
 if __name__ == "__main__":
